299ccf8b51963334924ba28226d9797ac01ea570
joice-flores <joicefloreslazarino@gmail.com>
2023-05-30 16:34:59 -0300

    remove cometarios

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -10,6 +10,7 @@ const Contatos = () => {
 					className="linkWhats"
 					href="https://wa.me/5532984172073?text=Gostaria de fazer um orçamento"
 					target="_blank"
+          rel="noreferrer"
 				>
 					<Button
 						variant="outlined"
@@ -19,33 +20,6 @@ const Contatos = () => {
 					</Button>
 				</a>
 			</Grid>
-			{/* <form>
-        <div className='form-box'>
-          <Grid container className='form-item'>
-            <Grid item className='form-input'>
-              <label htmlFor="fullName">Nome Completo:</label>
-              <input type="text" id="fullName" autoComplete='on' required />
-            </Grid>
-            <Grid item className='form-input'>
-              <label htmlFor="phone">Telefone:</label>
-              <input placeholder='(00)00000-0000' type="tel" id="phone" autoComplete='on' required />
-            </Grid>
-            <Grid item className='form-input'>
-              <label htmlFor="email">E-mail:</label>
-              <input type="email" id="Email" autoComplete='on' required placeholder='seuemail@mail.com' />
-            </Grid>
-            <Grid  item className='form-input'>
-              <label htmlFor="text">Mensagem:</label>
-              <textarea cols={50} rows={10}
-                id="mensagem" required />
-            </Grid>
-            <Grid className='form-buttons'>
-              <input type="reset" value='Limpar' />
-              <input type="submit" value='Enviar' />
-            </Grid>
-          </Grid>
-        </div>
-      </form> */}
 		</Grid>
 	);
 };
